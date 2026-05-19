355192ef29fd5836fabf8ed449067c7d4ba20df6
Mayur Helkar <mayurhelkar@yahoo.com>
2026-05-19 12:58:13 +0530

    Add files via upload

---
+++ Preprocessor/Comment_MulitLine/comment_multiLine.c
@@ -0,0 +1,9 @@
+/*
+gcc -save-temps -o program program.c
+*/
+
+int main()
+{
+    return 0; /* returns zero 
+    On Successful completion of main function */ 
+}
---
+++ Preprocessor/Comment_MulitLine/comment_multiLine.i
@@ -0,0 +1,13 @@
+# 0 ".\\comment_multiLine.c"
+# 0 "<built-in>"
+# 0 "<command-line>"
+# 1 ".\\comment_multiLine.c"
+
+
+
+
+int main()
+{
+    return 0;
+
+}
