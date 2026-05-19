fdf1c08c1036d6218827249e8d43d56e3faa7e63
Mayur Helkar <mayurhelkar@yahoo.com>
2026-05-19 12:36:32 +0530

    Add files via upload

---
+++ ExtensionSupported/C/ExtensionSupported.c
@@ -0,0 +1,15 @@
+/*
+int main()  return 0; // error: expected declaration specifiers before 'return'
+                      // error: expected '{' at end of input
+*/
+
+/*
+int main()  
+return 0;  // error: expected declaration specifiers before 'return'
+           // error: expected '{' at end of input
+*/
+
+int main()
+{
+    return 0;
+}  
