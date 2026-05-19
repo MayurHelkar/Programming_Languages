fb4e963bc599b50271473454f31ff3b178bb4ea6
Mayur Helkar <mayurhelkar@yahoo.com>
2026-05-19 13:12:54 +0530

    Add files via upload

---
+++ Preprocessor/DefineInclusion/MacroDouble/macroDouble.i
@@ -0,0 +1,14 @@
+# 0 ".\\macroDouble.c"
+# 0 "<built-in>"
+# 0 "<command-line>"
+# 1 ".\\macroDouble.c"
+
+
+
+
+double main()
+{
+    double var = 9876.54321 ;
+
+    return 0;
+}
---
+++ Preprocessor/DefineInclusion/MacroDouble/macroDouble.s
@@ -0,0 +1,28 @@
+	.file	"macroDouble.c"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movsd	.LC0(%rip), %xmm0
+	movsd	%xmm0, -8(%rbp)
+	pxor	%xmm0, %xmm0
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section .rdata,"dr"
+	.align 8
+.LC0:
+	.long	-2014855058
+	.long	1086540357
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (Rev2, Built by MSYS2 project) 16.1.0"
